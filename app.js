10461993a2c9fce9ab36ffa0d2e051f394422cee
Md. Nahid Hasan <nahid7532140@gmail.com>
2024-08-16 18:39:20 +0600

    fetch from api

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const searchBtn = document.getElementById('searchBtn');
 
 //check input object command
 //console.dir(searchInput)
+//get data using arrow function
+// const getData=()=>{
+//     console.log(searchInput.value)
+// }
 
-searchBtn.addEventListener("click",function(){
-    console.log("click")
-})
+searchBtn.addEventListener("click",getData)
 
+function getData(){
+    const searchValue = searchInput.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`
+    console.log()
 
+    fetch(url)
+    .then((res)=>res.json())  //which is return data or we can use (res)=>{return res.json()}
+    .then((data)=>{
+        console.log(data);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}
 
 
 
