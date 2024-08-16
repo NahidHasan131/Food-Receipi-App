15c6a1b8b9ad2c6c0f9b0fa6643bd96e7ea85ebe
Md. Nahid Hasan <nahid7532140@gmail.com>
2024-08-16 22:19:49 +0600

    fetch data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Dom element select
 const searchInput = document.getElementById('searchInput');
 const searchBtn = document.getElementById('searchBtn');
+const foodDisplayContainer = document.getElementById('foodDisplayContainer');
 
 //check input object command
 //console.dir(searchInput)
@@ -19,9 +20,60 @@ function getData(){
     fetch(url)
     .then((res)=>res.json())  //which is return data or we can use (res)=>{return res.json()}
     .then((data)=>{
-        console.log(data);
+      displayCards(data.meals);
     })
     .catch((error)=>{
       console.log(error)
     });
 }
+
+function displayCards(data){
+  let childHtml = "";
+  for (const item of data) {
+    const { strMealThumb, strMeal, strInstructions, idMeal } = item;
+
+    let html = ` <div class="card card-compact bg-base-100 shadow-xl">
+              <figure>
+                <img class="w-full h-60 object-cover" src=${strMealThumb} alt="images"/>
+              </figure>
+            <div class="card-body">
+              <h2 class="card-title">${strMeal}</h2>
+              <p>
+                ${strInstructions.slice(0, 100)}
+              </p>
+              <div class="card-actions justify-end" onclick="modalFn(${idMeal})"}>
+                <label for="my-modal-6" class="btn btn-warning text-white">View Details</label>
+              </div>
+            </div>
+          </div> `;
+
+    childHtml = childHtml + html;
+  }
+
+
+  foodDisplayContainer.innerHTML = childHtml;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
