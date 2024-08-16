da2880b6ed5a176b260169a047ee47f190058dcc
Md. Nahid Hasan <nahid7532140@gmail.com>
2024-08-16 12:48:09 +0600

    js dom

---
+++ app.js
@@ -0,0 +1,29 @@
+//Dom element select
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+
+//check input object command
+//console.dir(searchInput)
+
+searchBtn.addEventListener("click",function(){
+    console.log("click")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
